c1f4edb05d08ccb55158b1c282c84f26587d3980
Andrei Perevozchikov <perevozchikov.andrey@gmail.com>
2026-07-04 13:34:55 +0300

    Add Product Base tab to sidebar

---
+++ src/components/ProductBasePage.js
@@ -0,0 +1,13 @@
+// src/components/ProductBasePage.js
+import React from 'react';
+
+const ProductBasePage = () => {
+  return (
+    <div>
+      <h1>База продуктов</h1>
+      <p>Список всех продуктов будет отображаться здесь.</p>
+    </div>
+  );
+};
+
+export default ProductBasePage;
---
+++ src/pages/product-base.js
@@ -0,0 +1,9 @@
+// src/pages/product-base.js
+import React from 'react';
+import ProductBasePage from '../components/ProductBasePage';
+
+const ProductBase = () => {
+  return <ProductBasePage />;
+};
+
+export default ProductBase;
