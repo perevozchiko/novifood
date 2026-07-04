4da036e380507271e18491452000307ed1d12be7
Andrei Perevozchikov <perevozchikov.andrey@gmail.com>
2026-07-04 14:15:39 +0300

    Revert "Add Product Base tab to sidebar"
    This reverts commit c1f4edb05d08ccb55158b1c282c84f26587d3980.

--- src/pages/product-base.js
+++
@@ -1,9 +0,0 @@
-// src/pages/product-base.js
-import React from 'react';
-import ProductBasePage from '../components/ProductBasePage';
-
-const ProductBase = () => {
-  return <ProductBasePage />;
-};
-
-export default ProductBase;
